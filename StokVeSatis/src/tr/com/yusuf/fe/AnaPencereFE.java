d39b0134fa8e9d7f64ff7b043838c37f71e04cec
Yusuf Aksoy <yusufaksoy.mdbf19@iste.edu.tr>
2022-11-02 16:40:28 +0300

    Menu ve menuitem eklemeleri yapildi.

--- StokVeSatis/src/tr/com/yusuf/fe/AnaPencereFE.java
+++ StokVeSatis/src/tr/com/yusuf/fe/AnaPencereFE.java
@@ -1,5 +1,6 @@
 package tr.com.yusuf.fe;
 import javax.swing.JFrame;
+import javax.swing.JMenuItem;
 import javax.swing.JMenu;
 import javax.swing.JPanel;
 import javax.swing.JMenuBar;
@@ -12,7 +13,7 @@ public class AnaPencereFE extends JFrame implements FeInterfaces {
 	@Override
 	public void initPencere() {
 		JTabbedPane tabs = new JTabbedPane();
-		JMenuBar bar = new JMenuBar();
+		JMenuBar bar = initBar();
 		//add(tabs);
 		setJMenuBar(bar);//
 		setTitle("Satýþ ve Stok Programý");
@@ -24,13 +25,31 @@ public class AnaPencereFE extends JFrame implements FeInterfaces {
 	}
 	@Override
 	public JPanel initPanel() {
-		return null;
+		JPanel panel = new JPanel();
+		return panel;
 	}
 	@Override
 	public JMenuBar initBar() {
 		JMenuBar bar = new JMenuBar();
 		JMenu dosyaMenu = new JMenu("Dosya");
 		bar.add(dosyaMenu);
+		JMenuItem jMenuItemCikis = new JMenuItem("Çýkýþ");
+		dosyaMenu.add(jMenuItemCikis);
+		JMenu urunlerMenu = new JMenu("Ürünler");
+		bar.add(urunlerMenu);
+		JMenuItem urunEkleItem = new JMenuItem("Ürün Ekle");
+		urunlerMenu.add(urunEkleItem);
+		JMenuItem kategoriEkleItem = new JMenuItem("Kategori Ekle");
+		urunlerMenu.add(kategoriEkleItem);
+		urunlerMenu.addSeparator();
+		JMenuItem urunDuzenleItem = new JMenuItem("Ürün düzenle");
+		urunlerMenu.add(urunDuzenleItem);
+		JMenuItem kategoriDuzenleItem = new JMenuItem("Kategori Düzenle");
+		urunlerMenu.add(kategoriDuzenleItem);
+		
+		
+		
+		
 		return bar;
 	}
 	@Override
