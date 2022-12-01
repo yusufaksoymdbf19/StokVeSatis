34774cfd948a32d863877e5e902b8d286f1d76db
Yusuf Aksoy <yusufaksoy.mdbf19@iste.edu.tr>
2022-12-01 21:32:04 +0300

    kategori ekleme islemi yapiliyor

--- StokVeSatis/src/tr/com/yusuf/fe/KategoriEkleFE.java
+++ StokVeSatis/src/tr/com/yusuf/fe/KategoriEkleFE.java
@@ -10,11 +10,18 @@ import tr.com.yusuf.interfaces.FeInterfaces;
 public class KategoriEkleFE extends JDialog implements FeInterfaces {
 
 	public KategoriEkleFE() {
-		
+		initPencere();
 	}
 
 	@Override
 	public void initPencere() {
+		setTitle("Kategori Ekle");
+		pack();
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setModalityType(DEFAULT_MODALITY_TYPE);
+		setDefaultCloseOperation(HIDE_ON_CLOSE);
+		
 		
 		
 	}
